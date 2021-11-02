f9569fb4e7e1b5094b4843fa9745c4b536a4af00
milsandaya <milaryandaya@gmail.com>
2021-11-01 22:59:58 -0500

    Search added

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -8,4 +8,9 @@ class RestaurantController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
   end
+
+  # def search
+  #   wildcard_search = "%#{params[:keywords]}%"
+  #   @restaurants = Restaurant.where("name LIKE ?", wildcard_search)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   get "/restaurants/:id", to: "restaurant#show"
   get "/about", to: "restaurant#about"
 
+  # resources :restaurants, only: %i[index show] do
+  #   collection do
+  #     get "search"
+  #   end
+  # end
+
   # get "/order", to: "order#index"
   # get "/order/:id", to: "order#show"
   # resources :restaurants do
